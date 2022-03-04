586a4295e244b087c0ea2a394aca07595783a4dc
Sahandgha <sahand.gha@gmail.com>
2022-03-05 08:33:33 +1000

    complete the result section and intall html react parser

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Parser from "html-react-parser"
 
 export default function SearchResults({results}) {
   return (
     <div className="w-full mx-auto px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
         <p className="text-gray-600 text-sm mb-5 mt-3">About {results.searchInformation.formattedTotalResults} results ({results.searchInformation.formattedSearchTime} seconds)</p>
+        {results.items.map((result)=>(
+            <div key={result.link} className="max-w-xl mb-8">
+                    <div className="group">
+                        <a className="text-sm truncate"  href={result.link}>{result.formattedUrl}</a>
+                        <a className="group-hover:underline decoration-blue-800" href={result.link}>
+                            <h2 className="truncate text-xl font-medium text-blue-800">{result.title}</h2>
+                        </a>
+                    </div>
+                    <p className="text-gray-600">{Parser(result.htmlSnippet)}</p>
+            </div>
+        ))}
     </div>
   )
 }
